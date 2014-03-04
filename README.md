96b474dd956e357eee53c16d3bd8213866abc913
Kirk gleason <kgleason@bloommg.com>
2014-03-04 13:59:55 -0500

    Added install.sh

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Some of the more important dot files from my homedirectory.
 I've removed the dots from the names so that they do not thump any existing files.
 
 I've also split my .bash_profile out into a .bash_profile.d directory. Makes it easier to find things when I want to tweak them.
+
+Running ./install.sh should put all of these files in the right place for you. Of course if you know what you are doing, then you will probably be better off doing this manually.
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Create a variable to hold the date. We'll use this for making backups before we overwrite anything
+MYDATE=$(date +%Y%m%d)
+
+# Warn the user about what we are about to do. Give them a chance to opt out
+echo "I am about to make backups of and replace some of your dotfiles. Press <Ctrl>+C to abort now. Enter to continue."
+read PAUSE
+
+for f in bash_profile bashrc gitconfig gitignore_global gvimrc
+do
+	if [ -e ~/.${f} ]
+	then
+		cp ~/.${f} ~/.${f}.${MYDATE}.orig
+		rm -f ~/.${f}
+	fi
+	
+	cp ${f} ~/.${f}
+done
+
+for d in bash_profile.d vim
+do
+	if [ -d ~/.${d} ]
+	then
+		cp -r ~/.${d} ~/.${d}.${MYDATE}.orig
+		rm -rf ~/.${d}
+	fi
+	
+	cp -r ${d} ~/.${d}
+done
