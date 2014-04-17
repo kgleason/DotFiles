6acfca67aa84ddd57bb369cafccb9bb321e0dded
Kirk gleason <kgleason@bloommg.com>
2014-04-17 16:37:22 -0400

    Added in rbenv stuff. Homebrew should now also complain less about the path.

--- bash_profile.d/environment.sh
+++ bash_profile.d/environment.sh
@@ -6,3 +6,10 @@
 
 # Add my local bin directory to my path if it exists
 [[ -d "$HOME/bin" ]] && PATH=${PATH}:~/bin
+
+# Add some ruby environmental stuff
+eval "$(rbenv init -)"
+
+# And fix the paths for homebrew to be happy
+declare -x HOMEBREW_PATH=/usr/local/bin:/usr/local/sbin
+declare -x PATH=$HOMEBREW_PATH:$PATH
