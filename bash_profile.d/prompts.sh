59172b200e5826410c539f4720943dd3a39a99cb
Kirk gleason <kgleason@bloommg.com>
2014-06-09 13:21:13 -0400

    Added in user@host for my sanity.

--- bash_profile.d/prompts.sh
+++ bash_profile.d/prompts.sh
@@ -8,4 +8,4 @@ PS1_OLD=${PS1}
 #Read in some git stuff
 source ~/.bash_profile.d/git-prompt
 
-PS1="\[$Yellow\]\w\[\033[m\]\[$IPurple\]\$(__git_ps1)\[$Blue\]\$ \[$IGreen\]"
+PS1="\[$BCyan\]\u@\h:\[$Yellow\]\w\[\033[m\]\[$IPurple\]\$(__git_ps1)\[$Blue\]\$ \[$IGreen\]"
