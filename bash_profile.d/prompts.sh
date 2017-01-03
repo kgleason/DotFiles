062d659eee78678762ac70d9d1f04bf6c32e2236
Kirk Gleason <kgleason@gmail.com>
2017-01-03 14:11:16 -0500

    Added in optional powerline config

--- bash_profile.d/prompts.sh
+++ bash_profile.d/prompts.sh
@@ -9,3 +9,11 @@ PS1_OLD=${PS1}
 source ~/.bash_profile.d/git-prompt
 
 PS1="\[$BCyan\]\u@\h:\[$Yellow\]\w\[\033[m\]\[$IPurple\]\$(__git_ps1)\[$Blue\]\$ \[$IGreen\]"
+
+#If powerline is installed, then use it instead
+if command -v powerline-daemon > /dev/null; then
+	powerline-daemon -q
+	POWERLINE_BASH_CONTINUATION=1
+	POWERLINE_BASH_SELECT=1
+	source /usr/lib/python3.5/site-packages/powerline/bindings/bash/powerline.sh
+fi
