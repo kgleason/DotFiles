d41572b855ced8fe4c393a1e5af8ad965463d73b
Kirk Gleason <kgleason@bloominsurance.com>
2019-10-14 16:02:53 -0400

    Fixed an issue with ruby not existing.

--- bash_profile.d/environment.sh
+++ bash_profile.d/environment.sh
@@ -8,15 +8,16 @@
 [[ -d "$HOME/bin" ]] && PATH=${PATH}:~/bin
 
 # Add some ruby environmental stuff
-export RBENV_ROOT=/usr/local/var/rbenv
-#which rbenv 2>&1 >> /dev/null
-#RBENVX=$?
-[[ -e $(which rbenv &>/dev/null) ]] && eval "$(rbenv init -)"
+RUBY=$(which ruby &> /dev/null)
+[[ -e ${RUBY} ]] && {
+	export RBENV_ROOT=/usr/local/var/rbenv
+	[[ -e $(which rbenv &>/dev/null) ]] && eval "$(rbenv init -)"
 
-[[ -d $(ruby -e 'print Gem.user_dir') ]] && PATH="$PATH:$(ruby -e 'print Gem.user_dir')/bin"
+	[[ -d $(ruby -e 'print Gem.user_dir') ]] && PATH="$PATH:$(ruby -e 'print Gem.user_dir')/bin"
+}
 
 # And fix the paths for homebrew to be happy
-[[ -e $(which brew) &>/dev/null ]] && declare -x HOMEBREW_PATH=/usr/local/bin:/usr/local/sbin; declare -x PATH=$HOMEBREW_PATH:$PATH
+[[ -e $(which brew &>/dev/null) ]] && declare -x HOMEBREW_PATH=/usr/local/bin:/usr/local/sbin; declare -x PATH=$HOMEBREW_PATH:$PATH
 
 # Keep less output on the screen
 # This seems to break a lot of things in a WSL environment, so we'll disable it
