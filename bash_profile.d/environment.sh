abc88297f5754921fbd78f2f33426cd1e0e8432b
Kirk Gleason <kgleason@bloominsuranceagency.com>
2019-10-30 08:17:44 -0400

    Removed the LESS=X env variable. Also broken in WSL.

--- bash_profile.d/environment.sh
+++ bash_profile.d/environment.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
-
 # Path adjustments
 # Add the Android SDK stuff to my path if it exists
 [[ -d "$HOME/Library/Developer/Xamarin/android-sdk-mac_x86" ]] && PATH=${PATH}:${HOME}/Library/Developer/Xamarin/android-sdk-mac_x86/platform-tools:${HOME}/Library/Developer/Xamarin/android-sdk-mac_x86/tools
-
 # Add my local bin directory to my path if it exists
-[[ -d "$HOME/bin" ]] && PATH=${PATH}:~/bin
-
+[[ -d "$HOME/bin" ]] && PATH="${PATH}:${HOME}/bin"
 # Add some ruby environmental stuff
-export RBENV_ROOT=/usr/local/var/rbenv
-#which rbenv 2>&1 >> /dev/null
-#RBENVX=$?
 [[ -e $(which rbenv &>/dev/null) ]] && eval "$(rbenv init -)"
-
 [[ -d $(ruby -e 'print Gem.user_dir') ]] && PATH="$PATH:$(ruby -e 'print Gem.user_dir')/bin"
-
 # And fix the paths for homebrew to be happy
-[[ -e $(which brew) &>/dev/null ]] && declare -x HOMEBREW_PATH=/usr/local/bin:/usr/local/sbin; declare -x PATH=$HOMEBREW_PATH:$PATH
-
+[[ -e "$(which brew) &> /dev/null" ]] && declare -x HOMEBREW_PATH=/usr/local/bin:/usr/local/sbin; declare -x PATH=$HOMEBREW_PATH:$PATH
 # Keep less output on the screen
-export LESS="X"
+#export LESS="X"
